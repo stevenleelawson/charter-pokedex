8e84c198b738e578d41ccfa16850a0555047b557
Steven Lawson <unluckygamblermusic@gmail.com>
2020-03-03 13:24:59 -0700

    reorganizing files, initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">hellloooo</div>
+    )
+  }
+}
+
+export default App;
