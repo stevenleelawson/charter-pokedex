fa47cac2f7144684cc9a3c053a377ab9587f35a8
Steven Lawson <unluckygamblermusic@gmail.com>
2020-03-04 16:51:40 -0700

    adding types and weaknesses to each card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,11 +3,26 @@ import './styles.css';
 
 const Card = ({ pokemon }) => {
   console.log('pokemon', pokemon)
+  const types = pokemon.type.map(type => (
+    <p>{type}</p>
+  ));
+  const weaknesses = pokemon.weaknesses.map(weakness => <p>{weakness}</p>);
   return (
     <div className="card">
       <h1>{pokemon.name}</h1>
       <p>id: {pokemon.id}</p>
       <img src={pokemon.img} alt={pokemon.name} />
+      <div className="details-container">
+        <div>
+          <h6>Types:</h6>
+          {types || ""}
+        </div>
+        <div>
+          <h6>Weaknesses:</h6>
+          {weaknesses || ""}
+        </div>
+        {/* <p>Weaknesses: {weaknesses || ""}</p> */}
+      </div>
     </div>
   );
 }
