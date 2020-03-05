c10ba0e3c2382b01bb0909eba5f29061015127bd
Steven Lawson <unluckygamblermusic@gmail.com>
2020-03-04 17:14:18 -0700

    refactored PokemonContainer to have state to be used for filtering

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,10 @@ import './styles/App.css';
 import PokemonContainer from './PokemonContainer/PokemonContainer';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      pokemon: [],
-    }
-  }
-  componentDidMount() {
-    const url = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
-    fetch(url)
-      .then(response => response.json())
-      .then(data => this.setState({
-        pokemon: data.pokemon
-      }))
-  }
   render() {
     return (
       <div className="App">
-        <PokemonContainer data={this.state.pokemon} />
+        <PokemonContainer />
       </div>
     )
   }
--- src/components/PokemonContainer/PokemonContainer.js
+++ src/components/PokemonContainer/PokemonContainer.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card/Card';
 import './styles.css';
 
-const PokemonContainer = ({ data }) => {
-  let pokemon;
-  
-  if (data) {
-    pokemon = data.map((pokemon, i) => (
-      <div key={pokemon.name + i}>
-        <Card pokemon={pokemon} />
-      </div>
-      ))
+class PokemonContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      pokemon: []
+    };
+  }
+  componentDidMount() {
+    const url =
+      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+    fetch(url)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          pokemon: data.pokemon
+        })
+      );
+  }
+  render() {
+    console.log('poke', this.state.pokemon);
+    let pokemon;
+
+    if (this.state.pokemon) {
+      pokemon = this.state.pokemon.map((pokemon, i) => (
+        <div key={pokemon.name + i}>
+          <Card pokemon={pokemon} />
+        </div>
+      ));
     }
-  return (
-    <div className="card-container">{ pokemon || 'something broke' }</div>
-  );
+    return <div className="card-container">{pokemon || "something broke"}</div>;
+  }
 }
 
 export default PokemonContainer;
